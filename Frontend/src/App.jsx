dc10ad4fb3b33f41f4b28c91125986a14e619014
Rudra Choudhary <rudrachoudhary067@gmail.com>
2026-04-09 20:02:49 +0530

    Expression detected

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import FaceExpression from './features/Expression/components/FaceExpression'
+
+function App() {
+  
+
+  return (
+   <FaceExpression />
+  )
+}
+
+export default App
