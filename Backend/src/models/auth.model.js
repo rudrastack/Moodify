d3d71599a52b81d3652053fcf411f8da2375cb2f
Rudra Choudhary <rudrachoudhary067@gmail.com>
2026-04-11 18:46:52 +0530

    set-up backend

---
+++ Backend/src/models/auth.model.js
@@ -0,0 +1,23 @@
+const moongoose = require('mongoose');
+
+const userSchema = new moongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true    
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const UserModel = moongoose.model('User', userSchema);
+
+module.exports = UserModel;
--- Frontend/src/features/Expression/components/FaceExpression.jsx
+++ Frontend/src/features/Expression/components/FaceExpression.jsx
@@ -1,95 +1,46 @@
 import { useEffect, useRef, useState } from "react";
-import {FaceLandmarker,FilesetResolver} from "@mediapipe/tasks-vision";
+import { detect, init } from "../utils/utils";
 
 
-export default function FaceExpression() {
-const videoRef = useRef(null);
-const landmarkerRef = useRef(null);
-const animationRef = useRef(null);
-let stream;
-const [expression, setExpression] = useState("Detecting...");
+export default function FaceExpression({ onClick = () => { } }) {
+    const videoRef = useRef(null);
+    const landmarkerRef = useRef(null);
+    const streamRef = useRef(null);
+
+    const [ expression, setExpression ] = useState("Detecting...");
+
+    useEffect(() => {
+        init({ landmarkerRef, videoRef, streamRef });
+
+        return () => {
+            if (landmarkerRef.current) {
+                landmarkerRef.current.close();
+            }
+
+            if (videoRef.current?.srcObject) {
+                videoRef.current.srcObject
+                    .getTracks()
+                    .forEach((track) => track.stop());
+            }
+        };
+    }, []);
+
+    async function handleClick() {
+        const expression = detect({ landmarkerRef, videoRef, setExpression })
+        console.log(expression)
+        onClick(expression)
+    }
 
 
-const init = async () => {
-const vision = await FilesetResolver.forVisionTasks(
-"https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
-);
-landmarkerRef.current = await FaceLandmarker.createFromOptions(
-vision,
-{
-baseOptions: {
-modelAssetPath:
-"https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task",
-},
-outputFaceBlendshapes: true,
-runningMode: "VIDEO",
-numFaces: 1
-}
-);
-stream = await navigator.mediaDevices.getUserMedia({ video: true });
-videoRef.current.srcObject = stream;
-await videoRef.current.play();
-detect();
-};
-
-const detect = () => {
-if (!landmarkerRef.current || !videoRef.current) return;
-const results = landmarkerRef.current.detectForVideo(
-videoRef.current,
-performance.now()
-);
-if (results.faceBlendshapes?.length > 0) {
-const blendshapes = results.faceBlendshapes[0].categories;
-const getScore = (name) =>
-blendshapes.find((b) => b.categoryName === name)?.score || 0;
-const smileLeft = getScore("mouthSmileLeft");
-const smileRight = getScore("mouthSmileRight");
-const jawOpen = getScore("jawOpen");
-const browUp = getScore("browInnerUp");
-const frownLeft = getScore("mouthFrownLeft");
-const frownRight = getScore("mouthFrownRight");
-let currentExpression = "Neutral";
-if (smileLeft > 0.5 && smileRight > 0.5) {
-currentExpression = "Happy 😄";
-} else if (jawOpen > 0.6 && browUp > 0.5) {
-currentExpression = "Surprised 😲";
-} else if (frownLeft > 0.01 && frownRight > 0.01) {
-currentExpression = "Sad 😢";
-}
-setExpression(currentExpression);
-}
-animationRef.current = requestAnimationFrame(detect);
-};
-
-useEffect(() => {
-
-
-init();
-
-return () => {
-if (animationRef.current) {
-cancelAnimationFrame(animationRef.current);
-}
-if (landmarkerRef.current) {
-landmarkerRef.current.close();
-}
-if (videoRef.current?.srcObject) {
-videoRef.current.srcObject
-.getTracks()
-.forEach((track) => track.stop());
-}
-};
-}, []);
-
-return (
-<div style={{ textAlign: "center" }}>
-<video
-ref={videoRef}
-style={{ width: "400px", borderRadius: "12px" }}
-playsInline
-/>
-<h2>{expression}</h2>
-<button onClick={detect}>Detect Expression</button>
-</div>
-);
+    return (
+        <div style={{ textAlign: "center" }}>
+            <video
+                ref={videoRef}
+                style={{ width: "400px", borderRadius: "12px" }}
+                playsInline
+            />
+            <h2>{expression}</h2>
+            <button onClick={handleClick} >Detect expression</button>
+        </div>
+    );
 }
