ce915f8fc70b70d4e79edcca4449c60489ee2b90
Rudra Choudhary <rudrachoudhary067@gmail.com>
2026-04-14 22:26:01 +0530

    auth done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
-
+const authRoutes = require('./routes/auth.route');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 app.use(express.json());
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+app.use(cookieParser());
+app.use('/api/auth', authRoutes);
 
 
 module.exports = app;
